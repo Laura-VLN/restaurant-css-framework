89dff38e93d4e47bd364e4d14bb13183a082eae0
ShinYami <loriisconti@gmail.com>
2021-01-28 14:13:42 +0100

    Update form.js

--- styles/form.js
+++ styles/form.js
@@ -15,7 +15,7 @@ function validateForm()
    
     if (name.value == "")                                  
     { 
-        document.getElementById('errorname').innerHTML="Veuillez entrez un nom valide";  
+        document.getElementById('errorname').innerHTML="Enter a valid name";  
         name.focus(); 
         return false; 
     }else{
@@ -24,7 +24,7 @@ function validateForm()
        
     if (email.value == "")                                   
     { 
-        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        document.getElementById('erroremail').innerHTML="Enter a valide Email Adress"; 
         email.focus(); 
         return false; 
     }else{
@@ -33,21 +33,21 @@ function validateForm()
    
     if (email.value.indexOf("@", 0) < 0)                 
     { 
-        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        document.getElementById('erroremail').innerHTML="Enter a valide Email Adress"; 
         email.focus(); 
         return false; 
     } 
    
     if (email.value.indexOf(".", 0) < 0)                 
     { 
-        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        document.getElementById('erroremail').innerHTML="Enter a valide Email Adress"; 
         email.focus(); 
         return false; 
     } 
    
     if (message.value == "")                           
     {
-        document.getElementById('errormsg').innerHTML="Veuillez entrez un message valide"; 
+        document.getElementById('errormsg').innerHTML="Enter a valid message"; 
         message.focus(); 
         return false; 
     }else{
